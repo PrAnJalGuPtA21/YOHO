f252ccf31202f3e94e06826b7a36b56de1120300
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-08-27 16:44:35 +0530

    complain form added

--- frontend/src/components/Complainform.jsx
+++ frontend/src/components/Complainform.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Complainform() {
+    
+    var id=123
+    const [user,setUser]=useState({
+        name:"", email:"", hostelname:"", phone:"", unique_id:"", issue:"", time:"", roomnumber:"", description:""
+    });
+    let name,value;
+    const handleInputs=(e)=>{
+        e.preventDefault();
+        // console.log(e);
+        name=e.target.name;
+        value=e.target.value;
+        setUser({...user,[name]:value});
+        console.log(user);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+         id=id+1;
+        setUser({...user,unique_id:id});
+        
+        console.log(user)
+        
+    }
+
+
+
   return (
-    <div>Complainform</div>
+    <div>
+        <form action="submit" className="form" onClick={handleSubmit}>
+            <input type="text" name='name' id='name' autoComplete='off' value={user.name} onChange={handleInputs} placeholder='Name' />
+            <input type="email" name='email' id='email' autoComplete='off' value={user.email} onChange={handleInputs} placeholder='Email' />
+            <input type="text" name='hostelname' id='hostelname' autoComplete='off' value={user.hostelname} onChange={handleInputs} placeholder='Hostel Name' />
+            <input type="phonenumber" name='phone' id='phone' autoComplete='off' value={user.phone} onChange={handleInputs} placeholder='Phone Number' />
+            <input type="text" name='issue' id='issue' autoComplete='off' value={user.issue} onChange={handleInputs} placeholder='Issue' />
+            <input type="number" name='roomnumber' id='roomnuber' autoComplete='off' value={user.roomnumber} onChange={handleInputs} placeholder='Room' />
+            <input type="text" name='description' id='description' autoComplete='off' value={user.description} onChange={handleInputs} placeholder='Description' />
+            <input type="submit" name="submit" id='submit' />
+        </form>
+    </div>
   )
 }
 
