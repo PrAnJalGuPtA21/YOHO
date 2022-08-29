39eeac801d38c2481040a75ec8d5e147882068ea
greko <gyanratan636@gmail.com>
2022-08-29 11:20:52 +0530

    Corrections

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,17 @@ const app = require("./app");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
-//Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
 });
 
-// Config
+
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-// Connecting to database
 connectDatabase();
 
 cloudinary.config({
@@ -27,7 +25,6 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-// Unhandled Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
